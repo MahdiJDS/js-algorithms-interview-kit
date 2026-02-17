de262439f36fe8ed33e9afba8dc09c1c4dc7b446
Mahdi Jahed <mahdijahed56@gmail.com>
2026-02-17 08:04:52 +0330

    feat: Add answer as a comment

--- basics/regex.js
+++ basics/regex.js
@@ -1,9 +1,9 @@
 //___________Regex
 
 let email = "-.test@example.com";
-console.log(/^[\w.-]+@[a-z]+\.[a-z]{2,}$/.test(email));
+console.log(/^[\w.-]+@[a-z]+\.[a-z]{2,}$/.test(email)); //true
 
 
 let phone = "09121234567";
-console.log(/^09\d{9}$/.test(phone));
+console.log(/^09\d{9}$/.test(phone)); //true
 
