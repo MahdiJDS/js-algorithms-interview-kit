07e751f698fee02b81c848d8408a30d87825ee53
Mahdi Jahed <mahdijahed56@gmail.com>
2026-02-17 08:15:47 +0330

    feat: Add O(n²) Two Sum solution using nested loops

---
+++ arrays/two-sum.js
@@ -0,0 +1,16 @@
+//_________two sum
+
+//1
+function twoSum(nums, target) {
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] === target) {
+                return [i, j];
+            }
+        }
+    }
+    return null;
+}
+
+const result = twoSum([2, 7, 11, 15], 17);
+console.log(result);
