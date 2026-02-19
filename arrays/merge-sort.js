cdec5f377111f6d83791915eec221d6a779d8a31
Mahdi Jahed <mahdijahed56@gmail.com>
2026-02-19 08:20:05 +0330

    feat: Implement merge logic for two sorted arrays

---
+++ arrays/merge-sort.js
@@ -0,0 +1,20 @@
+//_______Merge Two Sorted Array
+
+function marge(arr1, arr2) {
+    let i = 0;
+    let j = 0;
+    let result = [];
+
+    while (i < arr1.length && j < arr2.length) {
+        if (arr1[i] < arr2[j]) {
+            result.push(arr1[i]);
+            i++;
+        } else {
+            result.push(arr2[j]);
+            j++;
+        }
+    }
+    return result.concat(arr1.slice(i)).concat(arr2.slice(j))
+}
+
+console.log(marge([1, 3, 5], [2, 4, 6, 8]));
