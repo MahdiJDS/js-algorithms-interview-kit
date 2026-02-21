4b31b05b1322177b689e81ee0bd5a21c70ec0857
Mahdi Jahed <mahdijahed56@gmail.com>
2026-02-21 09:11:15 +0330

    Add answer as a comment(remove-duplicates.js)

--- arrays/remove-duplicates.js
+++ arrays/remove-duplicates.js
@@ -7,4 +7,4 @@ function repite(arr) {
     const deleteR = arr.filter((item, index) => arr.indexOf(item) == index);
     return deleteR;
 }
-console.log(repite([1, 2, 2, 1, 5]))
+console.log(repite([1, 2, 2, 1, 5])) // [ 1, 2, 5 ]
