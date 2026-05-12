c2a26efd2ce4bdcaf27b253cab73b77180466f26
Mahdi Jahed <mahdijahed56@gmail.com>
2026-05-12 09:50:35 +0330

    feat: Add new regex for URL regex test

--- basics/regex.js
+++ basics/regex.js
@@ -7,3 +7,7 @@ console.log(/^[\w.-]+@[a-z]+\.[a-z]{2,}$/.test(email)); //true
 let phone = "09121234567";
 console.log(/^09\d{9}$/.test(phone)); //true
 
+
+let Url = "https://github.com/MahdiJDS"
+const regex = /^(https?:\/\/)?([\da-z.-]+)\.([a-z.]{2,})([/\w.-]*)*\/?$/;
+console.log(regex.test(Url)) //true
